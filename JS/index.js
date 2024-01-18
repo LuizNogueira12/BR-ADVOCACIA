9c73d051b29d58c00a1917031289bd598cf98f71
Luiz Fernando Ramos Nogueira <103154491+LuizNogueira12@users.noreply.github.com>
2024-01-18 20:42:06 -0300

    corrigdo bug do ico humburguer

--- JS/index.js
+++ JS/index.js
@@ -2,8 +2,9 @@ function menushow(){
     let menuMobile = document.querySelector('.mobile-menu');
     if(menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "Img_header/close_white_36dp.svg";
+        document.querySelector('.icon').src = "Img_header/menu_white_36dp.svg";
     } else {
         menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "Img_header/close_white_36dp.svg"
     }
 }
